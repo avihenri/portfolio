8127b4be696aea9fd3916ef46c9fee1f69f0ba2d
AviHenri <avril.m.henry@gmail.com>
2019-07-28 00:20:44 +0100

    header started

---
+++ Avril.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Avril Henry | Portfolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-free-5.9.0-web/css/all.css">
+    <link rel="stylesheet" href="assets/css/portfolio-style.css">
+    <script defer src="assets/css/fontawesome-free-5.9.0-web/js/all.js"></script>
+  </head>
+  <body>
+   <?php // require 'inc/navbar.php'?>  
+  <section class="section section-top">  
+
+      <div class="columns" style="height: 100%;">
+
+          <div class="column flex-central-container">            
+            <div class="header-text">
+              <h1 class="title white">Avril Henry</h1>
+              <p class="subtitle white">Portfolio</p>
+            </div>           
+          </div>
+
+          <div class="column flex-central-container header-img">          
+           <!-- <div class="box-border"></div>
+            <div class="box-border2"></div> -->
+            <div class="header-img">
+              <img src="assets/imgs/m5.jpg" alt="Photo of me">        
+          </div>
+
+      </div>
+
+  </section>
+
+  </body>
+</html>

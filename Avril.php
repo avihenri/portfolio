1df9c9f1ebcbe49d9e73b34b7c289d25aa09460e
AviHenri <avril.m.henry@gmail.com>
2019-08-05 18:24:48 +0100

    added timeline

--- Avril.php
+++ Avril.php
@@ -7,7 +7,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
     <link rel="stylesheet" href="assets/css/fontawesome-free-5.9.0-web/css/all.css">
     <link rel="stylesheet" href="assets/css/portfolio-style.css">
-    <script defer src="assets/css/fontawesome-free-5.9.0-web/js/all.js"></script>
+    <!-- <link rel="stylesheet" href="/node-modules/bulma-timeline/css/bulma-timeline.min.css"> -->
+    <script rel="stylesheet" src="assets/css/fontawesome-free-5.9.0-web/js/all.js"></script>
+    
+    
   </head>
   <body>
    <?php // require 'inc/navbar.php'?>  
@@ -39,22 +42,74 @@
   </section>
 
   <section class="section section-details">
-  
+
+        <div class="aboutme ">
+          <h2 class="center-text">About Me</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+        <div class="columns">
+        <div class="column skills">
+        <h3 class="center-text">Skills</h3>
+        </div>
+        <div class="column tools">
+        <h3 class="center-text">Tools</h3>
+        </div>
+      </div>
+     
   
 
   </section>
-
+<!-- 
   <section class="section section-timeline">
+  <div class="timeline is-centered">
+  <header class="timeline-header">
+    <span class="tag is-medium is-primary">Start</span>
+  </header>
+  <div class="timeline-item is-primary">
+    <div class="timeline-marker is-primary"></div>
+    <div class="timeline-content">
+      <p class="heading">January 2016</p>
+      <p>Timeline content - Can include any HTML element</p>
+    </div>
+  </div>
+  <div class="timeline-item is-warning">
+    <div class="timeline-marker is-warning is-image is-32x32">
+      <img src="http://bulma.io/images/placeholders/32x32.png">
+    </div>
+    <div class="timeline-content">
+      <p class="heading">February 2016</p>
+      <p>Timeline content - Can include any HTML element</p>
+    </div>
+  </div>
+  <header class="timeline-header">
+    <span class="tag is-primary">2017</span>
+  </header>
+  <div class="timeline-item is-danger">
+    <div class="timeline-marker is-danger is-icon">
+      <i class="fa fa-flag"></i>
+    </div>
+    <div class="timeline-content">
+      <p class="heading">March 2017</p>
+      <p>Timeline content - Can include any HTML element</p>
+    </div>
+  </div>
+  <header class="timeline-header">
+    <span class="tag is-medium is-primary">End</span>
+  </header>
+</div> -->
 
 
   </section>
 
   <section class="section section-github">
-
+  <h3> <span><i class="fab fa-github"></i></span> GitHub Contributions</h3>
     
       <div class="flex-central-container">      
+      
       <img src="http://ghchart.rshah.org/EC9B06/avihenri" class="box" alt="Avihenri's Github chart" />  
     </div>
+    <h3><a href="https://github.com/avihenri" class="white-link">Check out my GitHub here</a></h3>
  
 
   </section>
